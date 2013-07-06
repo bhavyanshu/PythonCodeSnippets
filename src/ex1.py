da4e87b1bc59e983fe9f25ee8dafb599ad8cc0bd
bhavyanshu <bhavyanshu.spl@gmail.com>
2013-07-06 19:10:53 +0530

    Some more printing

--- src/ex1.py
+++ src/ex1.py
@@ -39,5 +39,27 @@ print"If I add cats & kittens, I would get %d."%(cats+kittens) #Oh yes you can d
 
 #Some more string vars examples.
 hilarious=False
-joke_evaluation="Isn't that joke so funny?! %r"
+joke_evaluation="Isn't that joke so funny?! %r" # Use the %r for debugging, since it displays the "raw" data of the variable
 print joke_evaluation%hilarious #So simple.
+
+print"Mary had a little lamb."
+print"Its fleece was white as %s."%'snow'
+print"And everywhere that Mary went."
+print"."*10 # what'd that do? It would print "." character 10 times.
+
+end1="C"
+end2="h"
+end3="e"
+end4="e"
+end5="s"
+end6="e"
+end7="B"
+end8="u"
+end9="r"
+end10="g"
+end11="e"
+end12="r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1+end2+end3+end4+end5+end6,
+print end7+end8+end9+end10+end11+end12
